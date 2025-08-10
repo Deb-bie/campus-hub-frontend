32c7adb507da56abf284b57fa85b9296a71cd8f8
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-08-09 22:05:04 -0500

    added home and auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { Register } from './components/register/register';
 import { Login } from './components/login/login';
+import { Home } from './components/home/home';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {
         path: '',
+        component: Home,
+        title: 'Home page',
+        canActivate: [authGuard]
+    },
+    {
+        path: 'register',
         component: Register,
         title: 'Register page'
     },
