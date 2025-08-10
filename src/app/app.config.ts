3e7a571cf496fd802d9f157349acdca56ecedae7
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-08-09 22:02:02 -0500

    config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient} from '@angular/common/http';
+import { provideHttpClient, withFetch} from '@angular/common/http';
+import { provideIcons } from '@ng-icons/core'
+import { heroBell, heroCalendarDays, heroClock, heroMapPin, heroUser, heroUserGroup, heroXMark } from '@ng-icons/heroicons/outline'
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient()
+    provideHttpClient(withFetch()),
+    provideIcons({heroBell, heroUser, heroMapPin, heroCalendarDays, heroClock, heroUserGroup, heroXMark })
   ]
 };
