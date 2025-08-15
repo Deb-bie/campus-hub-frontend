caa50d98ea133008ba478a367e68633f7b3c9a09
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-08-14 23:38:05 -0500

    loading current user on app start up

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { AuthService } from './services/auth';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,24 @@ import { RouterOutlet, RouterModule } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   title = 'Campus Hub';
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const storedToken = localStorage.getItem('authToken');
+
+      if (storedToken != null) {
+        this.authService.loadCurrentUser(storedToken).subscribe();
+        
+      }
+    } 
+    
+  }
+  
 }
